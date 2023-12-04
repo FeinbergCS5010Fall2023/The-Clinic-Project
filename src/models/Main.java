e2e90431290f83d116a8a50efd2486c928e0e23f
Michael Eshak <mikey@Michaels-MacBook-Pro-9.local>
2023-12-04 18:32:54 -0500

    Fixed the window issue

--- src/models/Main.java
+++ src/models/Main.java
@@ -2,83 +2,73 @@ package models;
 import javax.swing.*;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.InputStreamReader;
 
 public class Main {
+
     private static JFrame frame;
-    private static JPanel contentPanel;
+    private static JPanel contentPane;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             frame = new JFrame("The Clinic Project");
-            frame.setSize(400, 300);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-            JMenuBar menuBar = new JMenuBar();
-            JMenu fileMenu = new JMenu("File");
-
-            JMenuItem loadFileItem = new JMenuItem("Load Clinic File");
-            loadFileItem.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    loadClinicFile();
-                }
-            });
-
-            JMenuItem clearRecordsItem = new JMenuItem("Clear Records");
-            clearRecordsItem.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    clearRecords();
-                }
-            });
-
-            JMenuItem quitItem = new JMenuItem("Quit");
-            quitItem.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.exit(0);
-                }
-            });
-
-            contentPanel = createAboutPanel();
-            frame.getContentPane().add(contentPanel);
-
-            fileMenu.add(loadFileItem);
-            fileMenu.add(clearRecordsItem);
-            fileMenu.add(quitItem);
-            menuBar.add(fileMenu);
-
+            JMenuBar menuBar = createMenuBar();
             frame.setJMenuBar(menuBar);
 
-            // Comment the line below if you want to show the About dialog on startup
-            // showAboutDialog();
+            contentPane = createAboutPanel();
+            frame.setContentPane(contentPane);
 
+            frame.setMinimumSize(new Dimension(300, 300)); // Set minimum size
+
+            frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
     }
 
-    private static void loadClinicFile() {
-        JFileChooser fileChooser = new JFileChooser();
-        int result = fileChooser.showOpenDialog(frame);
+    private static JMenuBar createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
 
-        if (result == JFileChooser.APPROVE_OPTION) {
-            File selectedFile = fileChooser.getSelectedFile();
-            Readable input = new InputStreamReader(System.in);
-            Appendable output = System.out;
-            new ClinicConsoleController(input, output).playGame(new Clinic("Test"), selectedFile);
-        }
+        JMenu fileMenu = new JMenu("File");
+
+        JMenuItem loadFileItem = new JMenuItem("Load Clinic File");
+        loadFileItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadClinicFile();
+            }
+        });
+
+        JMenuItem clearRecordsItem = new JMenuItem("Clear Records");
+        clearRecordsItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearRecords();
+            }
+        });
+
+        JMenuItem quitItem = new JMenuItem("Quit");
+        quitItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        fileMenu.add(loadFileItem);
+        fileMenu.add(clearRecordsItem);
+        fileMenu.add(quitItem);
+
+        menuBar.add(fileMenu);
+
+        return menuBar;
     }
 
-    private static void clearRecords() {
-        // Add logic to clear records in memory
-        // This could involve creating a method in Clinic or ClinicConsoleController
-        // to reset or clear the records.
-        System.out.println("Records cleared.");
-    }
     private static JPanel createAboutPanel() {
         JPanel aboutPanel = new JPanel();
         aboutPanel.setLayout(new BorderLayout());
@@ -87,7 +77,6 @@ public class Main {
         aboutTextPane.setContentType("text/html");
         aboutTextPane.setEditable(false);
 
-        
         String aboutText = "<html><body style='font-family: Arial, sans-serif; padding: 10px;'>";
         aboutText += "<h2>Welcome to the Clinic Game!</h2>";
         aboutText += "<p>To play the game, click on Files, load the clinic files, and start playing! It's a beautiful day to save lives!<p>";
@@ -99,10 +88,32 @@ public class Main {
         aboutText += "</body></html>";
 
         aboutTextPane.setText(aboutText);
-        aboutPanel.add(aboutTextPane, BorderLayout.CENTER);
+        aboutPanel.add(new JScrollPane(aboutTextPane), BorderLayout.CENTER);
 
         return aboutPanel;
+    }
 
-  }
+    private static void loadClinicFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showOpenDialog(frame);
 
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            
+            Clinic clinic = new Clinic("Test"); // Replace with your logic to load the clinic from the file
+            new ClinicConsoleController(new InputStreamReader(System.in), System.out).displayGame(clinic);
+
+            // You may also want to update the about panel or perform other actions
+            contentPane.add(createAboutPanel(), BorderLayout.CENTER);
+            frame.revalidate();
+            frame.repaint();
+        }
+    }
+
+    private static void clearRecords() {
+        // Add logic to clear records in memory
+        // This could involve creating a method in Clinic or ClinicConsoleController
+        // to reset or clear the records.
+        JOptionPane.showMessageDialog(frame, "Records cleared.", "Records Cleared", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
